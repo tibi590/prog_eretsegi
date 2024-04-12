4b3f31e96c229f9e6ace49c1820f796facb4e5dc
tibi590 <pasztitibike@gmail.com>
2024-04-12 11:57:46 +0200

    Fixed things

--- 2022_maj/robot.py
+++ 2022_maj/robot.py
@@ -12,10 +12,10 @@ def main() -> None:
 def print_chars(prompt: str) -> None:
     # Initialize a dictionary for the directions
     chars = {
-        "E": 0,
-        "D": 0,
-        "K": 0,
-        "N": 0
+        'E': 0,
+        'D': 0,
+        'K': 0,
+        'N': 0
     }
 
     # Iterates through the prompt and 
@@ -30,41 +30,41 @@ def print_chars(prompt: str) -> None:
 def update_route(prompt: str) -> str:
     # Initialize a dictionary for the final position 
     coordinate: dict = {
-        "x": 0,
-        "y": 0
+        'x': 0,
+        'y': 0
     }
 
     # Calculate the final position 
     for char in prompt:
         match char:
-            case "E":
-                coordinate["y"] += 1;
-            case "D":
-                coordinate["y"] -= 1;
-            case "K":
-                coordinate["x"] += 1;
-            case "N":
-                coordinate["x"] -= 1;
+            case 'E':
+                coordinate['y'] += 1;
+            case 'D':
+                coordinate['y'] -= 1;
+            case 'K':
+                coordinate['x'] += 1;
+            case 'N':
+                coordinate['x'] -= 1;
 
     # Initialize a string for the new route 
     route: str = ""
 
     # Calculate the new route based on the final position
     # Checks if y coordinate is positive
-    if coordinate["y"] > 0:
-        # If true then append the "E" character y times 
-        route += "E"*coordinate["y"]
+    if coordinate['y'] > 0:
+        # If true then append the 'E' character y times 
+        route += 'E'*coordinate['y']
     else:
-        # Else append the "D" character abs(y) times 
-        route += "D"*abs(coordinate["y"])
+        # Else append the 'D' character abs(y) times 
+        route += 'D'*abs(coordinate['y'])
 
     # Checks if x coordinate is positive
-    if coordinate["x"] > 0:
-        # If true then append the "K" character x times 
-        route += "K"*coordinate["x"]
+    if coordinate['x'] > 0:
+        # If true then append the 'K' character x times 
+        route += 'K'*coordinate['x']
     else:
-        # Else append the "N" character abs(x) times 
-        route += "N"*abs(coordinate["x"])
+        # Else append the 'N' character abs(x) times 
+        route += 'N'*abs(coordinate['x'])
 
     return route
 
